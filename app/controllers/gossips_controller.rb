34223bf2459b8dfcdc3e4477c6c9809c29025e04
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-31 17:51:13 +0100

    [MAJ] méthodes show/edit/update/delete + views show/edit/update/delete

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,25 +1,55 @@
 class GossipsController < ApplicationController
 
 
-def index
-  
-end
+  def index
+    @gossips = Gossip.all
+  end
 
-def new
-end
+  def new
+    @gossip = Gossip.new
+  end
 
-def create
-  @gossip = Gossip.new
-  @gossip.anonymous_author = params[:anonymous_author]
-  @gossip.content = params[:content]
-  @gossip.save
+  def create
+    @gossip = Gossip.new(gossip_params)
+    # @gossip.anonymous_author = params[:anonymous_author]
+    # @gossip.content = params[:content]
+      if @gossip.save
+       redirect_to @gossip
+     else
+       render 'new'
+      end
 
-  redirect_to @gossip
+  end
 
-end
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
 
-def show
-  @gossip = Gossip.find(params[:id])
-end
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+   
+    redirect_to gossips_path
+  end
+
+  private
+
+    def gossip_params
+      params.require(:gossip).permit(:anonymous_author, :content)
+    end
 
 end
